c3e4168df83b17c4ca68ae18c795eb7aed3191a8
Jonathan Cheng <jcheng461@gmail.com>
2022-03-07 23:25:31 +0900

    add scripts that put the user input form values into one array. Use array prototype push to add these values into a new array. Display elements of the new array in HTML page as list items.

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,18 @@ $(document).ready(function () {
     const fav1 = $("input#favorite1").val();
     const fav2 = $("input#favorite2").val();
     const fav3 = $("input#favorite3").val();
+    const favArray = [fav1, fav2, fav3];
 
-    $(".favorite1").text(fav1);
-    $(".favorite2").text(fav2);
-    $(".favorite3").text(fav3);
+    // Practice targetting the elements in the array.
+    $(".favorite1").text(favArray[0]);
+    $(".favorite2").text(favArray[1]);
+    $(".favorite3").text(favArray[2]);
+
+    // Use "push()" to add only these elements to a brand new array.
+    const newFavArray = [];
+    newFavArray.push(favArray[0]);
+    newFavArray.push(favArray[1]);
+    newFavArray.push(favArray[2]);
 
     event.preventDefault();
   });
